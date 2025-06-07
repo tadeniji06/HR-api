b1aa44e20d9455994d7b55f859c26b1e0fc559d2
Debo Adeniji <tadeniji06@gmail.com>
2025-06-07 17:34:50 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,54 +1,67 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    position: {
+      type: String,
+      required: true,
+      enum: [
+        "Social Media Manager",
+        "Content Creator",
+        "Video Editor",
+        "Creative Designer",
+        "Content Creator",
+        "SEO",
+        "Other",
+        "Article writer",
+        "Administrator",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["staff", "admin"],
+      default: "staff",
+    },
+    department: {
+      type: String,
+      default: "Marketing",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  position: {
-    type: String,
-    required: true,
-    enum: ['Social Media Manager', 'Creative Designer', 'Content Creator', 'Marketing Specialist', 'Other']
-  },
-  role: {
-    type: String,
-    enum: ['staff', 'admin'],
-    default: 'staff'
-  },
-  department: {
-    type: String,
-    default: 'Marketing'
-  },
-  isActive: {
-    type: Boolean,
-    default: true
-  },
-  profilePicture: {
-    type: String,
-    default: ''
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -59,15 +72,15 @@ userSchema.pre('save', async function(next) {
 });
 
 // Compare password method
-userSchema.methods.comparePassword = async function(password) {
+userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
 // Remove password from JSON output
-userSchema.methods.toJSON = function() {
+userSchema.methods.toJSON = function () {
   const user = this.toObject();
   delete user.password;
   return user;
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
