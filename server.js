c3ba6f39fccbc6e959f07cf42d7f6d66910aae93
Debo Adeniji <tadeniji06@gmail.com>
2025-06-08 13:39:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://hr-360-staff.netlify.app/",
+    "https://hr-360-staff.netlify.app",
     // Add other allowed origins as needed
     // "https://yourdomain.com"
   ],
